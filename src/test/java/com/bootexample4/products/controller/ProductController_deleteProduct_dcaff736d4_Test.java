1d3773033164fe4d6b72fec49ac94c9725598728
roost-io <sudhir@zb.io>
2023-08-16 15:48:49 +0000

    Test generated by RoostGPT for test travagodemo using AI Type Open AI and AI Model gpt-4

---
+++ src/test/java/com/bootexample4/products/controller/ProductController_deleteProduct_dcaff736d4_Test.java
@@ -0,0 +1,8 @@
+<dependencies>
+    <dependency>
+        <groupId>com.missing.package</groupId>
+        <artifactId>missing-package</artifactId>
+        <version>1.0.0</version>
+    </dependency>
+    <!-- other dependencies -->
+</dependencies>
---
+++ src/test/java/com/bootexample4/products/controller/ProductController_updateProduct_9454a9af90_Test.java
@@ -0,0 +1,10 @@
+<project>
+  ...
+  <repositories>
+    <repository>
+      <id>custom-repo</id>
+      <url>http://url-to-your-custom-repo/</url>
+    </repository>
+  </repositories>
+  ...
+</project>
